e9ea2e7f8feac5fbf831f94263a8bf1422ceb759
Koustav Mishra <mkk6160@gmail.com>
2022-10-12 18:00:19 +0530

    DiscountRepository Added

---
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Discount.API.Entities;
+using System.Reflection.Metadata;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
